09857584ab07014675a0c8b8da8fcf36b0960ecb
Melisafitri <melisaf569@gmail.com>
2026-06-18 13:22:03 +0700

    feat: menambahkan horizontal list

---
+++ src/components/organisms/HorizontalNewsList/HorizontalNewsList.jsx
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import SmallCard from "../../molecules/SmallCard/SmallCard";
+import "./HorizontalNewsList.css";
+
+const HorizontalNewsList = ({ title = "", news = [] }) => {
+  const scrollRef = useRef(null);
+
+  const scroll = (dir) => {
+    scrollRef.current?.scrollBy({ left: dir === "left" ? -320 : 320, behavior: "smooth" });
+  };
+
+  if (news.length === 0) return null;
+
+  return (
+    <section className="horizontal-news">
+      <div className="horizontal-news__header">
+        <h2 className="horizontal-news__title">{title}</h2>
+        <div className="horizontal-news__arrows">
+          <button onClick={() => scroll("left")}>&#8249;</button>
+          <button onClick={() => scroll("right")}>&#8250;</button>
+        </div>
+      </div>
+      <div className="horizontal-news__list" ref={scrollRef}>
+        {news.map((item) => (
+          <div className="horizontal-news__item" key={item.id}>
+            <SmallCard
+              img={item.image}
+              title={item.title}
+              source={item.source}
+              description={item.description}
+              minutes={item.readingTime}
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default HorizontalNewsList;
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,19 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HomeTemplate from "../../templates/HomeTemplate/HomeTemplate";
 import BannerCarousel from "../../components/organisms/BannerCarousel/BannerCarousel";
 import NewsList from "../../components/organisms/NewsList/NewsList";
+import HorizontalNewsList from "../../components/organisms/HorizontalNewsList/HorizontalNewsList";
 import Pagination from "../../components/organisms/Pagination/Pagination";
 import TopicList from "../../components/organisms/TopicList/TopicList";
 import "./Home.css";
 
+const ITEMS_PER_PAGE = 10;
+
 const Home = () => {
+  const [slides, setSlides] = useState([]);
+  const [news, setNews] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [beritaUtama, setBeritaUtama] = useState([]);
+  const [terkini, setTerkini] = useState([]);
+  const [nasional, setNasional] = useState([]);
+  const [global, setGlobal] = useState([]);
+  const [ekonomi, setEkonomi] = useState([]);
+  const [olahraga, setOlahraga] = useState([]);
+  const [seleb, setSeleb] = useState([]);
+  const [gayaHidup, setGayaHidup] = useState([]);
+  const [otomotif, setOtomotif] = useState([]);
+  const [teknologi, setTeknologi] = useState([]);
+
+  useEffect(() => {
+    // TODO: fetch slides dari API
+  }, []);
+
+  useEffect(() => {
+    // TODO: fetch news dari API berdasarkan currentPage
+  }, [currentPage]);
+
+  useEffect(() => {
+    // TODO: fetch semua kategori dari API
+    // fetchCategory("berita-utama").then(setBeritaUtama)
+    // fetchCategory("terkini").then(setTerkini)
+    // fetchCategory("nasional").then(setNasional)
+    // fetchCategory("global").then(setGlobal)
+    // fetchCategory("ekonomi").then(setEkonomi)
+    // fetchCategory("olahraga").then(setOlahraga)
+    // fetchCategory("seleb").then(setSeleb)
+    // fetchCategory("gaya-hidup").then(setGayaHidup)
+    // fetchCategory("otomotif").then(setOtomotif)
+    // fetchCategory("teknologi").then(setTeknologi)
+  }, []);
+
   return (
     <HomeTemplate>
       <div className="home__layout">
         <div className="home__main">
-          <BannerCarousel />
-          <NewsList news={[]} />
-          <Pagination currentPage={1} totalPages={10} onPageChange={() => {}} />
+          <BannerCarousel slides={slides} />
+          <NewsList news={news} />
+          <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
+          <HorizontalNewsList title="Berita Utama" news={beritaUtama} />
+          <HorizontalNewsList title="Terkini" news={terkini} />
+          <HorizontalNewsList title="Nasional" news={nasional} />
+          <HorizontalNewsList title="Global" news={global} />
+          <HorizontalNewsList title="Ekonomi" news={ekonomi} />
+          <HorizontalNewsList title="Olahraga" news={olahraga} />
+          <HorizontalNewsList title="Seleb" news={seleb} />
+          <HorizontalNewsList title="Gaya Hidup" news={gayaHidup} />
+          <HorizontalNewsList title="Otomotif" news={otomotif} />
+          <HorizontalNewsList title="Teknologi" news={teknologi} />
         </div>
         <aside className="home__sidebar">
           <TopicList />
